0a0171744d307a76d852fe48df9bc50059c348e0
Romulo Lima <romulosilva-07@hotmail.com>
2021-08-30 16:01:45 -0300

    Active Record & Mass Assignment

--- routes/web.php
+++ routes/web.php
@@ -17,15 +17,72 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/queries/{event}', function($event){
+Route::get('/queries/{event?}', function($event = null){
     //$events = \App\Models\Event::all(); //select * from events
     //$events = \App\Models\Event::all(['title', 'description']); //select title, description from events
     //$event = \App\Models\Event::where('id', 1)->first();  //select * from where id = 1
-    $event = \App\Models\Event::find($event);  //select * from where id = 1
+//    $event = \App\Models\Event::find($event);  //select * from where id = 1
+
+    //insert into events(title, description, body, start_event) values(?, ?, ?, ?);
+    //Active Record - Inserção
+//    $event = new \App\Models\Event();
+//    $event->title = 'Evento via Eloquent e AR';
+//    $event->description = 'Descrição do Evento';
+//    $event->body = 'Conteúdo do evento';
+//    $event->start_event = date('Y-m-d H:i:s');
+//    $event->slug = \Illuminate\Support\Str::slug($event->title);
+
+    //update events set title = ?, body = ?, start_event = ?, slug = ? where id = ?;
+    //Active Record - Atualização
+//    $event = \App\Models\Event::find(31);
+//    $event->title = 'Evento Atualizado';
+//    $event->slug = \Illuminate\Support\Str::slug($event->title);
+//    $event->save(); //executando
+
+    //Atribuição Massa ou Mass Assignment
+//    $event = [
+//        'title' => 'Evento Atribuição em Massa',
+//        'description' => 'Descrição',
+//        'body' => 'Conteudo do evento',
+//        'slug' => 'evento-atribuicao-em-massa',
+//        'start_event' => date('Y-m-d H:i:s')
+//    ];
+
+//    return \App\Models\Event::create($event);
+
+    //Mass Update ou Atualização em Massa
+//    $eventData = [
+//        'title' => 'Evento Atribuição em Massa',
+//        'description' => 'Conteudo do evento atualizado com atualização em massa',
+//        'body' => 'Conteudo Atualizado mass update',
+//        'slug' => 'evento-atribuicao-em-massa',
+//        'start_event' => date('Y-m-d H:i:s')
+//    ];
+//
+//    $event = \App\Models\Event::find(32);
+//    $event->update($eventData);
+//    return $event;
+
+    //Delete Model via busca do model
+    //$event = \App\Models\Event::findOrFail(32);
+//    return $event->delete();
+
+    //Delete Models via ids... [30,31,21]
+    //return \App\Models\Event::destroy(29, 30, 31,);
+
+    return \App\Models\Event::orderBy('id', 'DESC')->limit(3)->get(); //select * from events order by id DESC limit 3;
 
     return $event;
 });
 
 Route::get('/hello-world', [\App\Http\Controllers\HelloWorldController::class, 'helloWorld']);
+
 //Setando 'name?' como não obrigatorio
 Route::get('/hello/{name?}', [\App\Http\Controllers\HelloWorldController::class, 'hello']);
+
+//Rotas CRUD base da base para eventos...
+Route::get('/events/index', [\App\Http\Controllers\EventController::class, 'index']);
+Route::get('/events/store', [\App\Http\Controllers\EventController::class, 'store']);
+Route::get('/events/update/{event}', [\App\Http\Controllers\EventController::class, 'update']);
+Route::get('/events/destroy/{event}', [\App\Http\Controllers\EventController::class, 'destroy']);
+
